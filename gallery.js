c1b630aed7c7556599a5e7d86eae6b1d28b4566e
Unknown <lsannar1@mail.csuchico.edu>
2017-04-25 11:52:50 -0700

    done with lab 24

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
-
-
+var chico=["Upper Park","North Rim","Bear Hole","Monkey Face","Bidwell Mansion"];
+var current;
 function shuffleGallery()
 {
+  var randomIndex;
+  do {
+    randomIndex= Math.floor(Math.random() * chico.length);
+  }
+  while (randomIndex==current );
+current=randomIndex;
 
+  console.log(chico[current]);
 }
